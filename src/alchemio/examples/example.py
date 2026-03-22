a9bd7e6a10b3a7a8a53a26f0d0cc537de6ff4d0d
Tom Demeyere <115232841+tomdemeyere@users.noreply.github.com>
2026-03-22 16:39:15 +0100

    Delete README.md content update template name (#2)

--- src/alchemio/examples/example.py
+++ src/alchemio/examples/example.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 
 def example_function() -> str:
     """An example function that returns a greeting."""
-    return "Hello from `template`!"
+    return "Hello from `alchemio`!"
--- tests/examples/test_example_function.py
+++ tests/examples/test_example_function.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from template.examples.example import example_function
+from alchemio.examples.example import example_function
 
 
 def test_example_function() -> None:
-    """Test the example_function from the template.examples module."""
+    """Test the example_function from the alchemio.examples module."""
     result = example_function()
 
     assert result.startswith("Hello from")
